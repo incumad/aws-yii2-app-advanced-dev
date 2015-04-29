1bf4ade55e52e1c59dc6b9f9b0aa9453be5a50fb
Javier Pérez <javierperezu@hotmail.com>
2015-04-29 16:26:51 +0200

    use enviroment to get a db conection

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -1,11 +1,20 @@
 <?php
+
+$dbHost = $_SERVER['RDS_HOSTNAME'];
+$dbPort = $_SERVER['RDS_PORT'];
+$dbName = $_SERVER['RDS_DB_NAME'];
+
+$dsn = "mysql:host={$dbHost};port={$dbPort};dbname={$dbName}";
+$username = $_SERVER['RDS_USERNAME'];
+$password = $_SERVER['RDS_PASSWORD'];
+
 return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => $dsn,
+            'username' => $username,
+            'password' => $password,
             'charset' => 'utf8',
         ],
         'mailer' => [
