d7915ee4f2dd7e6b6d150626223f4b43c1919dd8
Javier Pérez <javierperezu@hotmail.com>
2015-04-29 17:17:59 +0200

    db config from console and yii migrate command

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -1,12 +1,22 @@
 <?php
 
-$dbHost = $_SERVER['RDS_HOSTNAME'];
-$dbPort = $_SERVER['RDS_PORT'];
-$dbName = $_SERVER['RDS_DB_NAME'];
+if (isset($_SERVER)) {
+    // Web
+    $dbHost = $_SERVER['RDS_HOSTNAME'];
+    $dbPort = $_SERVER['RDS_PORT'];
+    $dbName = $_SERVER['RDS_DB_NAME'];
+    $username = $_SERVER['RDS_USERNAME'];
+    $password = $_SERVER['RDS_PASSWORD'];
+} else {
+    // Console
+    $dbHost = getenv('RDS_HOSTNAME');
+    $dbPort = getenv('RDS_PORT');
+    $dbName = getenv('RDS_DB_NAME');
+    $username = getenv('RDS_USERNAME');
+    $password = getenv('RDS_PASSWORD');
+}
 
 $dsn = "mysql:host={$dbHost};port={$dbPort};dbname={$dbName}";
-$username = $_SERVER['RDS_USERNAME'];
-$password = $_SERVER['RDS_PASSWORD'];
 
 return [
     'components' => [
